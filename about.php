adb2b6702835f5ecfec2c9e9e014a280a5e72d45
Talha POLAT <talhapolat@hotmail.com>
2022-04-01 17:54:25 +0300

    Hakkımızda sayfa güncellemesi.

--- about.php
+++ about.php
@@ -17,7 +17,7 @@ require_once("app/connect.php");
 	<!-- Title page -->
 	<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('images/bg-01.jpg');">
 		<h2 class="ltext-105 cl0 txt-center">
-			Hakkımızda
+			Hakkımızdaa
 		</h2>
 	</section>	
 
